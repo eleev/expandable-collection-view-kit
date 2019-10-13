76da165c28a522c1081a0b1645e0b6ddad6c2186
Astemir Eleev <astemireleev@gmail.com>
2019-10-13 13:10:45 +0300

    Updated licence info for test Tests

--- Tests/ExpandableCollectionViewKitTests/ExpandableCollectionViewKitTests.swift
+++ Tests/ExpandableCollectionViewKitTests/ExpandableCollectionViewKitTests.swift
@@ -1,15 +1,35 @@
+//
+//  ExpandableCollectionViewKitTests.swift
+//  ExpandableCollectionViewKit
+//
+//  Created by Astemir Eleev on 13.10.2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
 import XCTest
 @testable import ExpandableCollectionViewKit
 
 final class ExpandableCollectionViewKitTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ExpandableCollectionViewKit().text, "Hello, World!")
+    func testItem() {
+        let item = ExpandableItem(title: "Item")
+        XCTAssertEqual(item.title, "Item")
+        XCTAssertEqual(item.indentLevel, 0)
+        XCTAssertEqual(item.parent, nil)
     }
 
+    func testParentedItem() {
+        let folder = Folder(title: "Parent Folder") {
+            ExpandableItem(title: "Item")
+        }
+        
+        XCTAssertEqual(folder.title, "Parent Folder")
+        XCTAssertEqual(folder.indentLevel, 0)
+        XCTAssertEqual(folder.parent, nil)
+        XCTAssertEqual(folder.subitems.count, 1)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testItem", testItem),
+        ("testParentedItem", testParentedItem)
     ]
 }
--- Tests/ExpandableCollectionViewKitTests/XCTestManifests.swift
+++ Tests/ExpandableCollectionViewKitTests/XCTestManifests.swift
@@ -1,3 +1,11 @@
+//
+//  XCTestManifests.swift
+//  ExpandableCollectionViewKit
+//
+//  Created by Astemir Eleev on 13.10.2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
 import XCTest
 
 #if !canImport(ObjectiveC)
